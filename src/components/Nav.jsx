4a788c4b82fbfe40b108ca2c025233bb762e911c
Nell Flores <nell.djmf@gmail.com>
2022-04-15 14:34:25 -0400

    sign in form updates user, authenticated

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,31 @@
 import { Link } from 'react-router-dom'
 
 const Nav = ({ authenticated, user, handleLogOut }) => {
-  // let authenticatedOptions
-  // if (user) {
-  //   authenticatedOptions = (
-  //     <nav>
-  //       <h3>Welcome {user.email}!</h3>
-  //       <Link to="/dashboard">Dashboard</Link>
-  //       <Link onClick={handleLogOut} to="/">
-  //         Sign Out
-  //       </Link>
-  //     </nav>
-  //   )
-  // }
+  let authenticatedOptions
+  if (user) {
+    authenticatedOptions = (
+      <nav>
+        <h3>Welcome {user.email}!</h3>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link onClick={handleLogOut} to="/">
+          Sign Out
+        </Link>
+      </nav>
+    )
+  }
 
   const publicOptions = (
     <nav>
       <Link to="/">Home</Link>
       <Link to="/register">Register</Link>
       <Link to="/signin">Sign In</Link>
-			<Link to="/dashboard">Dashboard</Link>
+			{/* <Link to="/dashboard">Dashboard</Link>
 			<Link to="/browse_anime">Browse Anime</Link>
-			<Link to="/browse_lists">Browse Lists</Link>
+			<Link to="/browse_lists">Browse Lists</Link> */}
     </nav>
   )
+
+
 
   return (
     <header>
@@ -35,8 +37,7 @@ const Nav = ({ authenticated, user, handleLogOut }) => {
           />
         </div>
       </Link>
-      {/* {authenticated && user ? authenticatedOptions : publicOptions} */}
-			{publicOptions}
+      {authenticated && user ? authenticatedOptions : publicOptions}
     </header>
   )
 }
--- src/services/Auth.js
+++ src/services/Auth.js
@@ -2,10 +2,11 @@ import Client from './api'
 
 export const SignInUser = async (data) => {
   try {
-    const res = await Client.post('/auth/login', data)
+    // const res = await Client.post('/auth/login', data)
     // Set the current signed in users token to localStorage
-    localStorage.setItem('token', res.data.token)
-    return res.data.user
+    // localStorage.setItem('token', res.data.token)
+		localStorage.setItem('token', 'fake token')
+    // return res.data.user
   } catch (error) {
     throw error
   }
