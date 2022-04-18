81ba990d36ab0cc19bf553681b6290e2eeadec32
Nell Flores <nell.djmf@gmail.com>
2022-04-18 13:59:21 -0400

    moved anime useEffect to app.js

--- src/pages/AnimeDetail.js
+++ src/pages/AnimeDetail.js
@@ -1,11 +1,35 @@
 
 
-const AnimeDetail = () => {
+const AnimeDetail = ( {show}, {watchlist, clicked, isClicked, setWatchlist}) => {
 
 
   return (
-    <div className="anime-detail">
-			<h3>this is AnimeDetail</h3>
+		<div className="anime-item" key={show.id} style={{
+        '--poster-img': `url(${show.image})`
+    }}>
+
+    {
+      isClicked ? ( 
+        <div>
+          <div className='popup-container'>
+            <img src={show.image} alt='' onClick={()=>isClicked(false)} style={{
+              maxWidth: '600px',
+              border: '1px solid black'
+              }} className='popup'/>
+            </div>
+          <div className='overlay'></div>
+        </div>
+        ) : (
+          <div></div>
+        )
+      }
+      <div className='anime-card'>
+        <button onClick={()=>isClicked(true)}  className='item-name'>Show Info</button>
+        <button onClick={() => setWatchlist([...watchlist, show])}>Add to Watchlist</button>
+          <div className='info'>
+            <h3>title: { show.title } </h3>
+          </div>
+        </div>
     </div>
   )
 }
