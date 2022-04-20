34b99cdc860ca512cbfa0a630c73234868d80563
Nell Flores <nell.djmf@gmail.com>
2022-04-20 14:59:03 -0400

    tinkered with carousel

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,7 +1,16 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 
 const UserList = ({list}) => {
     const [clicked, isClicked] = useState(false)
+
+		const carouselElement = useRef(null)
+
+		const handleRight = () => {
+				document.getElementById('scroll').scrollRight += 1
+				console.log(document.getElementById('scroll'))
+				
+		}
+
     return (
         <div className="user-list-container">
             {
@@ -9,16 +18,20 @@ const UserList = ({list}) => {
                     <div className="popup-wrapper">
                         <h3>{list.username}'s Watchlist</h3>
                         <button onClick={() => isClicked(false)}>X</button>
-                        <div className="popup-info">
-                            {list.watch_list.map((listItem) => (
-                                <div className="list-wrapper">
-                                <h4>{listItem.title}
-                                <br />
-                                <img src={`${listItem.image}`}></img>
-                                </h4>
-                                </div>
-                            ))}
-                        </div>
+                        <div className="body">
+													<div className="carousel">
+                        		<div ref={carouselElement} className="popup-info carouselbox" id="scroll">
+                          	{list.watch_list.map((listItem) => (
+                              <h4>{listItem.title}
+                              <br />
+                              <img className="carousel-image" src={`${listItem.image}`}></img>
+                              </h4>
+                          	))}
+														<button className="switchLeft sliderButton"> Left </button> 
+          									<button className="switchRight sliderButton" onClick={()=> handleRight()} >Right</button> 
+													</div>
+												</div>
+											</div>
                     </div>
                 ) : (
                     <div></div>
