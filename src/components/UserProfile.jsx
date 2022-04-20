6017a795ef809d99a54d069571dcf997e07f157c
Ethan Nitz <etnitz@gmail.com>
2022-04-20 10:46:03 -0500

    render list working

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -6,15 +6,15 @@ const UserProfile = ({renderList}) => {
 			<h3>Your Profile</h3>
 				<h4>email: {localStorage.getItem('watcher')} </h4>
 				<div className="user-list">
-					<button onClick={() => (console.log(renderList[0]))}>test</button>
+					<button onClick={() => (console.log(renderList))}>test</button>
 					<h3>Current Watchlist</h3>
-					{renderList.map((newList) => {
-						<div className="user-list-wrapper" key={newList}>
+					{renderList.map((newList) => (
+						<div className="user-list-wrapper" key={newList.id}>
 						<li className="usersLists">
 							{newList.title}
 						</li>
 						</div>
-					})}
+					))}
 				</div>
 		</div>
 	)
