dd7e33de08d13ed1a32bc3bc3faa1999a9c339e7
Nell Flores <nell.djmf@gmail.com>
2022-04-21 15:19:53 -0400

    things

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-// export const BASE_URL = 'http://localhost:3001'
+
 export const apiUrl = process.env.NODE_ENV === 'production' ? 'https://mushi-roll.herokuapp.com/' : 'http://localhost:3001'
 console.log(apiUrl)
 
