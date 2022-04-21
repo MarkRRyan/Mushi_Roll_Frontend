870fd4f0d325fae9b7161d45672807e39b0902ac
Nell Flores <nell.djmf@gmail.com>
2022-04-21 02:44:23 -0400

    added seek functionality to music player

--- src/components/MusicControls.jsx
+++ src/components/MusicControls.jsx
@@ -1,12 +1,9 @@
 
-const MusicControls = ({ playing, isPlaying, skip, isSkipped }) => {
+const MusicControls = ({ playing, isPlaying, skip }) => {
 
 	return (
 		<div className="m-player-controls">
-			<button className="skip" onClick={()=>{
-				skip()
-				isSkipped(false)
-				}}>ᐊᐊ</button>
+			<button className="skip" onClick={()=>skip(false)}>ᐊᐊ</button>
 			<button className="play" onClick={()=>isPlaying(!playing)}>ᐅ</button>
 			<button className="skip" onClick={()=>skip()}>ᐅᐅ</button>
 			<br />
